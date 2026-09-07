e3b4a8bdf2eec7a9db74448497c0f775d80e3685
jediaelsantana31 <jediael.santana@gmail.com>
2026-09-07 11:10:39 -0300

    feat: inicializa projeto Music Stock

---
+++ src/main/java/com/desafio/java01/MusicStock.java
@@ -0,0 +1,10 @@
+package com.desafio.java01;
+
+
+public class MusicStock {
+    private static final int CAPACIDADE_MAXIMA_ESTOQUE = 100;
+
+    public static void main(String[] args) {
+        System.out.println(CAPACIDADE_MAXIMA_ESTOQUE);
+    }
+}
